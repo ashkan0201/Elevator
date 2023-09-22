b9145d18a95b05a595f25216fa2ecc8e4ea43188
ashkan0201 <ashkan02011@gmial.com>
2023-09-22 13:24:04 +0330

    added main code

--- Elavator.py
+++ Elavator.py
@@ -43,5 +43,86 @@ checkbox_states = [False] * len(checkbox_labels)
 d = {0:115 , 1:215 , 2:315 , 3:415 , 4:515 , 5:615}
 outsaid = []
 insaid = []
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            for i in range(len(checkbox_labels)):
+                checkbox_rect = pygame.Rect(checkbox_x, checkbox_y_start + i * checkbox_y_spacing, checkbox_width, checkbox_height)
+                if checkbox_rect.collidepoint(mouse_pos):
+                    checkbox_states[i] = not checkbox_states[i]
+                    try:
+                        if rect_y > d[i] > d[min(outsaid)] or rect_y < d[i] < d[max(outsaid)]:
+                            insaid.append(i)
+                    except:
+                        pass
+                    if rect_y >= d[i] and i not in insaid:
+                        outsaid.append(i)
+                    elif rect_y <= d[i] and i not in insaid:
+                        outsaid.append(i)
 
+
+    screen.fill((48, 52, 69))
+
+    number_y = (HEIGHT - number_font_size * len(numbers) - number_spacing * (len(numbers) - 1)) // 2
+    for number in numbers:
+        font = pygame.font.Font(None, number_font_size)
+        number_text = font.render(number, True, (2, 170, 255))
+        screen.blit(number_text, (number_x, number_y))
+        number_y += number_font_size + number_spacing
+
+    pygame.draw.rect(screen, (221, 144, 254), (rect_x, rect_y , rect_width, rect_height))
+    move_up = False
+    move_down = False
+    try:
+        if len(outsaid) != 0:
+            if d[outsaid[0]] < rect_y:
+                move_up = True
+                if len(insaid) != 0:
+                    insaid.sort()
+                    insaid.reverse()
+                    if rect_y == d[insaid[0]]:
+                        checkbox_states[insaid[0]] = not checkbox_states[insaid[0]]
+                        insaid.remove(insaid[0])
+                        sleep(2)
+            elif d[outsaid[0]] == rect_y:
+                checkbox_states[outsaid[0]] = not checkbox_states[outsaid[0]]
+                outsaid.remove(outsaid[0])
+                sleep(2)
+    
+        if len(outsaid) != 0:
+            if d[outsaid[0]] > rect_y:
+                move_down = True
+                if len(insaid) != 0:
+                    insaid.sort()
+                    if rect_y == d[insaid[0]]:
+                        checkbox_states[insaid[0]] = not checkbox_states[insaid[0]]
+                        insaid.remove(insaid[0])
+                        sleep(2)
+            elif d[outsaid[0]] == rect_y:
+                checkbox_states[outsaid[0]] = not checkbox_states[outsaid[0]]
+                outsaid.remove(outsaid[0])
+                sleep(2)
+        if move_up:
+            rect_y -= rect_speed
+        elif move_down:
+            rect_y += rect_speed
+    except:
+        pass
+    
+
+    for i in range(len(checkbox_labels)):
+        checkbox_rect = pygame.Rect(checkbox_x, checkbox_y_start + i * checkbox_y_spacing, checkbox_width, checkbox_height)
+        pygame.draw.rect(screen, WHITE, checkbox_rect)
+        if checkbox_states[i]:
+            pygame.draw.rect(screen, GREEN, checkbox_rect.inflate(-4, -4))
+        checkbox_font = pygame.font.Font(None, 20)
+        checkbox_text = checkbox_font.render(checkbox_labels[i], True, (255, 0, 81))
+        screen.blit(checkbox_text, (checkbox_x - checkbox_text.get_width() - checkbox_margin, checkbox_y_start + i * checkbox_y_spacing))
+    pygame.display.flip()
+    print(outsaid,insaid)
 pygame.quit()
